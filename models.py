aa0f69e99f37ea77a5cf65d9f47d9e4f147af446
Yenho Chen <yenhochen97@gmail.com>
2021-11-23 17:06:25 -0500

    add rnn and LR models

--- models.py
+++ models.py
@@ -70,3 +70,27 @@ class NN(nn.Module):
 #         sm = nn.functional.softmax(logits+10, dim=1) # numerically stable softmax
         return x
     
+
+class BiRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(BiRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm1 = nn.LSTM(input_size, hidden_size, 1, batch_first=True, bidirectional=True)
+        self.lstm2 = nn.LSTM(hidden_size*2, hidden_size*2, 1, batch_first=True, bidirectional=True)
+        self.fc1 = nn.Linear(hidden_size*4, 128)
+        self.fc2 = nn.Linear(128, num_classes)  # 2 for bidirection
+        
+        
+        
+    def forward(self, x):
+        # Set initial states
+        # Forward propagate LSTM
+#         out, _ = self.gru(x, (h0, c0))  # out: tensor of shape (batch_size, seq_length, hidden_size*2)
+        out, _ = self.lstm1(x)
+        out, _ = self.lstm2(out[:,:300,:])
+        
+        # Decode the hidden state of the last time step
+        out = nn.ReLU()(self.fc1(out[:,-1,:]))
+        out = self.fc2(out)
+        return out
