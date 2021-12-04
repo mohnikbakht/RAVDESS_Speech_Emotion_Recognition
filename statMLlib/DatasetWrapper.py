b76371f2e3460d1f0b814ca2d73da8e791057ba9
mohnikbakht <mohnikbakht97@gmail.com>
2021-12-04 17:45:38 -0500

    updated deep learning models

--- statMLlib/DatasetWrapper.py
+++ statMLlib/DatasetWrapper.py
@@ -15,7 +15,7 @@ class RAVDESSFeatureDataset(Dataset):
         self.mean_vec=mean
         self.std_vec=std
         self.feature_2d=feature_2d
-
+        
         if split=='train':
             if split_by=='actor':
                 self.path=root_dir+'split_by_actor/training/'
@@ -117,6 +117,41 @@ class RAVDESSFeatureDataset(Dataset):
                 for class_path in file_list:
                     self.data.append(class_path)
                 random.shuffle(self.data)
+                
+        elif split=='train_all':
+            if split_by=='actor':
+                self.path=root_dir+'split_by_actor/training_pot/'
+
+                file_list = glob.glob(self.path + "*")
+
+                self.data = []
+                for class_path in file_list:
+                    # class_name = class_path.split("/")[-1]
+                    for wave_path in glob.glob(class_path+"/*.pkl"):
+                        self.data.append(wave_path)
+                random.shuffle(self.data)
+            elif split_by=='statement':
+                self.path=root_dir+'split_by_statement/training_pot/'
+
+                file_list = glob.glob(self.path + "*")
+
+                self.data = []
+                for class_path in file_list:
+                    # class_name = class_path.split("/")[-1]
+                    for wave_path in glob.glob(class_path+"/*.pkl"):
+                        self.data.append(wave_path)
+                random.shuffle(self.data)
+                # print(self.data[0])
+            elif split_by=='arb':
+                self.path=root_dir+'arbitrary/training_pot/'
+
+                file_list = glob.glob(self.path + "/*.pkl")
+
+                self.data = []
+                for class_path in file_list:
+                    self.data.append(class_path)
+                random.shuffle(self.data)
+
             
         else:
             print(f"no data with split={split}")
@@ -139,13 +174,25 @@ class RAVDESSFeatureDataset(Dataset):
         # tonnetz=np.log(loaded.get("tonnetz")+1)
         
         if self.feature_2d:
-            #2d
-            src=np.r_[mfcc, chrom, mel]
+            #2d training data extractor
+            # 1) [mel]
+            # 2) [mfcc, mel]
+            # 3) [mfcc, chrom, mel]
+            # 4) [mfcc, chrom, contrast, mel]
+            src=np.r_[mfcc, chrom, contrast, mel]
             # src= mel
 
         else:
-            #1d
-            src=np.r_[mfcc, mel]
+            #1d training data extractor
+            global_features=loaded.get("global_features")
+            
+            # 1) [mel]
+            # 2) [mel, global_features]
+            # 2) [mfcc, mel, global_features]
+            # 3) [mfcc, chrom, mel, global_features]
+
+            src=np.r_[mfcc, mel, chrom, global_features]
+            
             src=np.reshape(src, (-1,1))
 
         # src=mel
--- statMLlib/FeatureExtraction.py
+++ statMLlib/FeatureExtraction.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 12 19:39:38 2021
-
-@author: skhan
-"""
 import os
 from sys import argv
 import re
@@ -18,112 +12,6 @@ import librosa
 import pickle
 
 
-def zeroPadFiles(root_dir):
-
-
-    # ---Filename identifiers---
-
-    Modality = '03'         #   (01 = full-AV, 02 = video-only, 03 = audio-only).
-    VocalChannel = '01'     #   (01 = speech, 02 = song).
-    Emotion = '04'          #   (01 = neutral, 02 = calm, 03 = happy, 04 = sad, 05 = angry,
-                            #   (06 = fearful, 07 = disgust, 08 = surprised)
-    EmotionalInt = '01'     #   (01 = normal, 02 = strong).
-    Statement = '01'        #   (01 = “Kids are talking by the door”, 02 = “Dogs are sitting by the door”).
-    Repetition = '01'       #   (01 = 1st repetition, 02 = 2nd repetition).
-    Actor = '20'            #   (01 to 24. Odd number =  male, even number =  female).
-
-    fileName = Modality +'-'+ VocalChannel +'-'+ Emotion +'-'+ EmotionalInt +'-'+ Statement +'-'+ Repetition +'-'+ Actor + '.wav'
-    rootDir = root_dir
-
-    emotionDict =	{
-      "01" : "neutral",
-      "02" : "calm",
-      "03" : "happy",
-      "04" : "sad",
-      "05" : "angry"
-    }
-
-    nFilesPerEmotion =	{
-      "neutral": 96,
-      "calm"   : 192,
-      "happy"  : 192,
-      "sad"    : 192,
-      "angry"  : 192
-    }
-
-    sampleRate = int(48e3);
-
-    nFilesPerActor = 60
-
-    actorMaleArray = [f"{v:02}" for v in list(range(1, 25, 2))]     # use if want to loop over male speeches
-    actorFemaleArray = [f"{v:02}" for v in list(range(2, 25, 2))]   # use if want to loop over female speeches
-    actorAll = [f"{v:02}" for v in list(range(1, 25, 1))]           # use if want to loop over all actors speeches
-    emotionAll =  [f"{v:02}" for v in list(range(1, 9, 1))] 
-    statementAll =  [f"{v:02}" for v in list(range(1, 3, 1))] 
-    repAll =  [f"{v:02}" for v in list(range(1, 3, 1))] 
-    emotionalInt =  [f"{v:02}" for v in list(range(1, 3, 1))] 
-
-    os.chdir(root_dir)
-    minSignalLen = int(140941)
-    maxSignalLen = int(419618)
-
-    extendSigFlag = 1
-    debugPoint = 0
-    lenVec = []
-
-    for Actor in actorAll:
-        for Emotion in emotionAll:
-            for EmotionalInt in emotionalInt:
-                for Statement in statementAll:
-                    for Repetition in repAll:
-
-                        if (Emotion == "01") and (EmotionalInt == "02"):
-                            EmotionalInt = "01"
-
-
-                        fileName = Modality +'-'+ VocalChannel +'-'+ Emotion +'-'+ EmotionalInt +'-'+ Statement +'-'+ Repetition +'-'+ Actor + '.wav'
-
-                        os.chdir(r"Actor_" + fileName[-6:-4])
-
-                        nFiles = os.walk(os.getcwd())
-
-                        # signalWav = wave.open(fileName, 'r')
-
-
-                        # Extract Raw Audio from Wav File
-                        
-                        signal, sampleRate = librosa.load(fileName)
-                        # signal = signalWav.readframes(-1)
-                        # signal = np.frombuffer(signal, "int16")
-
-                        lenVec.append(len(signal))
-
-                        os.chdir("../")
-
-                        if (extendSigFlag == 1):
-
-                            newpath = r"Actor_" + fileName[-6:-4] + "_extended"
-                            if not os.path.exists(newpath):
-                                os.makedirs(newpath)
-
-                            samplesToAppend = int(maxSignalLen - len(signal))      # extra samples
-
-                            if (not(samplesToAppend == 0)):
-                                signalExtended = np.append(signal, (np.zeros(samplesToAppend)))
-                            else:
-                                signalExtended = signal
-
-
-                            os.chdir(newpath)
-
-                            write(fileName[0:-4] + "_extended" ".wav", sampleRate, signalExtended.astype(np.int16))
-
-                            os.chdir("../")
-
-
-    stopVar = 0
-
-    
 def extractFeatures(root_dir, split_by, Feature_2D=False):    
 
     Modality = '03'         #   (01 = full-AV, 02 = video-only, 03 = audio-only).
@@ -164,9 +52,9 @@ def extractFeatures(root_dir, split_by, Feature_2D=False):
     actorMaleArray = [f"{v:02}" for v in list(range(1, 25, 2))]     # use if want to loop over male speeches
     actorFemaleArray = [f"{v:02}" for v in list(range(2, 25, 2))]   # use if want to loop over female speeches
     actorAll = [f"{v:02}" for v in list(range(1, 25, 1))]           # use if want to loop over all actors speeches
-    actorTrain = [f"{v:02}" for v in list(range(1, 25, 1))]
+    actorTrain = [f"{v:02}" for v in list(range(1, 21, 1))]
     actorTest = [f"{v:02}" for v in list(range(21, 25, 1))]
-    emotionAll =  [f"{v:02}" for v in list(range(1, 9, 1))] 
+    emotionAll =  [f"{v:02}" for v in list(range(2, 6, 1))] 
     statementAll =  [f"{v:02}" for v in list(range(1, 3, 1))] 
     repAll =  [f"{v:02}" for v in list(range(1, 3, 1))] 
     emotionalInt =  [f"{v:02}" for v in list(range(1, 3, 1))] 
@@ -181,23 +69,19 @@ def extractFeatures(root_dir, split_by, Feature_2D=False):
     lenVec = []
     
     # max_t=0
-    for Actor in actorTrain:
+    for Actor in (actorTrain+actorTest):
         print(f"Actor {Actor}")
         for Emotion in emotionAll:
             for EmotionalInt in emotionalInt:
                 for Statement in statementAll:
                     for Repetition in repAll:
-
+                        
+                        
+                            
                         if (Emotion == "01") and (EmotionalInt == "02"):
                             EmotionalInt = "01"
 
 
-                        
-                        # fileName = Modality +'-'+ VocalChannel +'-'+ Emotion +'-'+ EmotionalInt +'-'+ Statement +'-'+ Repetition +'-'+ Actor + '.wav'
-
-#                         os.chdir(r"Actor_" + fileName[-6:-4] + '_extended')
-
-#                         fileName = Modality +'-'+ VocalChannel +'-'+ Emotion +'-'+ EmotionalInt +'-'+ Statement +'-'+ Repetition +'-'+ Actor + '_extended' + '.wav'
                         
     
                         os.chdir(r"Actor_" + Actor )
@@ -206,30 +90,31 @@ def extractFeatures(root_dir, split_by, Feature_2D=False):
 
                         nFiles = os.walk(os.getcwd())
 
-                        # signalWav = wave.open(fileName, 'r')
 
                         # Extract Raw Audio from Wav File
-                        signal, sampleRate = librosa.load(fileName)
-#                         if (max_t<(len(signal)/sampleRate)):
-#                             max_t=(len(signal)/sampleRate)
-#                             print(f" {len(signal)}   {sampleRate}")
-                            
+                        signal, sampleRate = librosa.load(fileName)      
                         
                         
                         #pad zeros
                         max_len=116247
                         signal = np.pad(signal, (0, max_len-len(signal)), 'constant')
                         # print(len(signal)/sampleRate)
+                        
+                        
+                        
                         #normalize signal
                         signal = (signal-np.mean(signal))/np.std(signal)
                         
+                        #extract global features
+                        tmp_signal=signal.astype(float)
+                        global_features = [np.max(tmp_signal), np.min(tmp_signal), np.mean(tmp_signal), np.std(tmp_signal), np.median(tmp_signal), np.max(tmp_signal)-np.min(tmp_signal)]
                         # FEATURE EXTRACTION
                         
-                        
+                        win_len=1024#int(len(signal)/80)#2400
+                        hop_len=int(win_len/2)
                         if Feature_2D:
                         #2D feature
-                            win_len=1024#int(len(signal)/80)#2400
-                            hop_len=int(win_len/2)
+                            
 
                              # mfcc
                             mfccCoeffs = librosa.feature.mfcc(y=signal.astype(float), sr=sampleRate, n_fft=win_len, hop_length=hop_len, n_mfcc=40)
@@ -238,7 +123,7 @@ def extractFeatures(root_dir, split_by, Feature_2D=False):
                             chromaCoeffs = librosa.feature.chroma_stft(y=signal.astype(float), sr=sampleRate, n_fft=win_len, hop_length=hop_len)
 
                             # mel spectrogram
-                            melspectCoeffs = librosa.feature.melspectrogram(y=signal.astype(float), sr=sampleRate, n_fft=win_len, hop_length=hop_len, n_mels=128)
+                            melspectCoeffs = librosa.feature.melspectrogram(y=signal.astype(float), sr=sampleRate, n_fft=win_len, hop_length=hop_len, n_mels=160)
 
                             # contrast
                             contrastCoeffs = librosa.feature.spectral_contrast(y=signal.astype(float), sr=sampleRate, n_fft=win_len, hop_length=hop_len, n_bands=6)
@@ -249,8 +134,6 @@ def extractFeatures(root_dir, split_by, Feature_2D=False):
 
                         else: 
                             #1D feature
-                            win_len=int(len(signal)/40)#2400
-                            hop_len=int(win_len/2)
 
                             ## MFCCs
                             mfccCoeffs=np.mean(librosa.feature.mfcc(y=signal.astype(float), sr=sampleRate, n_mfcc=40), axis=1)
@@ -277,16 +160,20 @@ def extractFeatures(root_dir, split_by, Feature_2D=False):
                             # tonnetz
                             harmon = librosa.effects.harmonic(signal.astype(float))
                             tonnetz = librosa.feature.tonnetz(y=harmon, sr=sampleRate)
-
+                        
+                        
+                        
+                        
                         featureDict =	{
                           "ID" :   fileName[0:-13],
                           "emotion" : emotionDict[Emotion],
-                          "emotion_number" : int(Emotion)-1,
+                          "emotion_number" : int(Emotion)-2,
                           "mfccCoeffs": mfccCoeffs,
                           "chromaCoeffs"   : chromaCoeffs,
                           "melspectCoeffs"  : melspectCoeffs,
                           "contrastCoeffs"    : contrastCoeffs,
-                          "tonnetz"  : tonnetz
+                          "tonnetz"  : tonnetz,
+                          "global_features": global_features
                         }
 
                         os.chdir(root_dir)
@@ -303,12 +190,21 @@ def extractFeatures(root_dir, split_by, Feature_2D=False):
                         
                         if split_by=='actor':
                             # SAVE DATA
-                            actor_dir=f"split_by_actor/actor_{Actor}"
-                            
-                            if not os.path.exists(actor_dir):
-                                os.makedirs(actor_dir)
-                            with open(actor_dir+'/'+fileName[0:-4] + '_features.pkl', 'wb') as f:
-                                pickle.dump((featureDict), f)
+                            if Actor in actorTrain:
+                                actor_dir=f"split_by_actor/training_pot/actor_{Actor}"
+
+                                if not os.path.exists(actor_dir):
+                                    os.makedirs(actor_dir)
+                                with open(actor_dir+'/'+fileName[0:-4] + '_features.pkl', 'wb') as f:
+                                    pickle.dump((featureDict), f)
+                            if Actor in actorTest:
+                                actor_dir=f"split_by_actor/test/actor_{Actor}"
+
+                                if not os.path.exists(actor_dir):
+                                    os.makedirs(actor_dir)
+                                with open(actor_dir+'/'+fileName[0:-4] + '_features.pkl', 'wb') as f:
+                                    pickle.dump((featureDict), f)
+                                    
                         elif split_by=='statement':
                             # SAVE DATA
                             statement_dir=f'split_by_statement/actor_{Statement}'
@@ -319,11 +215,19 @@ def extractFeatures(root_dir, split_by, Feature_2D=False):
                                 
                         elif split_by=='arb':
                             # SAVE DATA
-                            statement_dir=f'arbitrary/all'
-                            if not os.path.exists(statement_dir):
-                                os.makedirs(statement_dir)
-                            with open(statement_dir+'/'+fileName[0:-4] + '_features.pkl', 'wb') as f:
-                                pickle.dump((featureDict), f)
+                            if Actor in actorTrain:
+                                statement_dir=f'arbitrary/training_pot'
+                                if not os.path.exists(statement_dir):
+                                    os.makedirs(statement_dir)
+                                with open(statement_dir+'/'+fileName[0:-4] + '_features.pkl', 'wb') as f:
+                                    pickle.dump((featureDict), f)
+                            elif Actor in actorTest:
+                                statement_dir=f'arbitrary/test'
+                                if not os.path.exists(statement_dir):
+                                    os.makedirs(statement_dir)
+                                with open(statement_dir+'/'+fileName[0:-4] + '_features.pkl', 'wb') as f:
+                                    pickle.dump((featureDict), f)
+                                
                         else:
                             print("error: not a valid split option")
                             return 1
